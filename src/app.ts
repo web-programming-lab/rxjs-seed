2b3167a0d41318fcf02fee5b4b7e16ca6fd328da
Andreas Schürmann <andreas@adhook.io>
2019-09-04 20:33:58 +0200

    Initial Commit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-let students = [
-  {"name": "Anna", "class": 4},
-  {"name": "Patrick", "class": 2},
-  {"name": "Claudio", "class": 3},
-  {"name": "Andreas", "class": 4},
-  {"name": "Klara", "class": 1},
-  {"name": "Kilian", "class": 2},
-  {"name": "Pius", "class": 1},
-];
+import {interval} from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
-// Create an Observable from students list and 
-// filter list for students in class 1. Print the values on console.
+
+var source = interval(400).pipe(
+  take(9), 
+  map(i => ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'][i]));
+
+// TODO: Create an Observable `result` that emits the 
+// sum of all numbers in source. Use pure functions
+// such as map, filter, reduce, scan, merge, delay, 
+// concat, take, etc.
+
+result.subscribe(x => console.log(x));
 
