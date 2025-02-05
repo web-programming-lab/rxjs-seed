357544fd8a29ff6cd5521d1ac1daa09faeeaf23d
p18750 <david.lichtsteiner@css.ch>
2025-02-05 22:08:40 +0100

    add slide code create 2

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import {Observable, Observer} from 'rxjs';
 const observable = new Observable((observer: Observer<any>) => {
-    observer.next('Hello');
-    observer.next('World');
+    observer.next('1');
+    observer.next('2');
+    observer.next('3');
     observer.complete();
+    observer.next('4');
 });
+observable.subscribe((value) => console.log(value));
 observable.subscribe((value) => console.log(value));
